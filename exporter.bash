1646e7eaae58284fd1606af1a7a609ffc82121fc
boichee <bhlevenson@gmail.com>
2015-04-16 22:40:46 -0700

    Added a header to exporter

---
+++ exporter.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Package: Exporter
+# File: /exporter.bash
+# Description:
+# This file does the main work of setting up the system that allows this application to
+# permanently add environment variables to my system
+
+# Need to parse out any arguments that get passed
+
+
